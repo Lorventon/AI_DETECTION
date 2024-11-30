daaea105b6298c87b4506de3c6989934b1ff6f2c
KRackL1n <karlov2006@bk.ru>
2024-12-01 01:39:43 +0300

    fix train model

--- model/train.py
+++ model/train.py
@@ -1,14 +1,7 @@
 # import torch
 from ultralytics import YOLO
-<<<<<<< Updated upstream
-model = YOLO("yolo11n-seg.pt")
-=======
 
 model = YOLO("yolo11s-seg.pt")
-
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
->>>>>>> Stashed changes
-
 # Запуск обучения
 result = model.train(
     data='D:/Projects/HackInHome2024/model/dataset/data.yaml',       # путь к файлу конфигурации датасета
@@ -16,11 +9,5 @@ result = model.train(
     imgsz=640,              # размер входного изображения
     batch=8,               # размер батча
     name='car_segmentation',# имя эксперимента
-<<<<<<< Updated upstream
     cache=True              # кэширование данных для ускорения
 )
-=======
-    cache=True,            # кэширование данных для ускорения
-)
-
->>>>>>> Stashed changes
