7fb8d2cb33bfade716e7d7064f7bc9dd608169a0
KRackL1n <karlov2006@bk.ru>
2024-12-01 05:14:11 +0300

    final model

--- model/train.py
+++ model/train.py
@@ -5,9 +5,9 @@ model = YOLO("yolo11s-seg.pt")
 # Запуск обучения
 result = model.train(
     data='D:/Projects/HackInHome2024/model/dataset/data.yaml',       # путь к файлу конфигурации датасета
-    epochs=25,             # количество эпох обучения
+    epochs=250,             # количество эпох обучения
     imgsz=640,              # размер входного изображения
-    batch=8,               # размер батча
+    batch=16,               # размер батча
     name='car_segmentation',# имя эксперимента
     cache=True              # кэширование данных для ускорения
 )
